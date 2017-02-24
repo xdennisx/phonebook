bc1178c6fe7eca8f80aea27437754d2e852090cf
xdennisx <denlee0125@gmail.com>
2017-02-24 15:44:52 +0800

    Modify the data structure
    * Comment the original data structure except lastName.
    * Use another data structure to store remain data.
    * And add a new pointer point to the new structure.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,8 +6,7 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 // #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct __OTHER_PHONE_BOOK_ENTRY {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +16,23 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+    struct __PHONE_BOOK_ENTRY *pNext;
+} other_entry;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    /*
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+    */
+    other_entry *otherInfomation;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
